687ba54ff9dc622d40ea3f398bb500393a8be25e
Grayside <aross@phase2technology.com>
2015-01-31 21:52:10 -0800

    Correct and expand test coverage.

--- README.md
+++ README.md
@@ -8,21 +8,37 @@ All oembed payload responses are valid option keys for the oEmbed handler.
 
 The 'html' and 'title' keys may be callbacks to facilitate data real-time data-loading.
 
+## Setting Up Routes
+
+In your oEmbed provider routes, you will want to add the following validation.
+(Assumes [Joi](http://github.com/hapijs/joi) library for route validation.)
+
+```js
+    validate: {
+        query: {
+            url: Joi.required(),
+            maxwidth: Joi.number().integer().min(1).default(600),
+            maxheight: Joi.number().integer().min(1).default(600),
+        }
+    }
+```
+
+
 ## Example
 ```js
-        handler: {
-            oembed: {
-                type: 'rich',
-                html: function(options, request) {
-                    return '<aside>This is the greatest</aside>'
-                },
-                title: function(options, request) {
-                    return 'Example',
-                },
-                provider_name: 'Example Content Provider',
-                provider_url: 'http://example.com'
-            }
-        },
+    handler: {
+        oembed: {
+            type: 'rich',
+            html: function(options, request) {
+                return '<aside>This is the greatest</aside>'
+            },
+            title: function(options, request) {
+                return 'Example',
+            },
+            provider_name: 'Example Content Provider',
+            provider_url: 'http://example.com'
+        }
+    }
 ```
 
 ## License
--- test/test.js
+++ test/test.js
@@ -9,16 +9,17 @@ var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
 
-describe('oEmbed handler', function () {
+describe('oEmbed', function() {
+  describe('handler', function() {
     var provisionServer = function() {
         var server = new Hapi.Server({minimal: true});
+        server.connection();
         server.register(oEmbed, Hoek.ignore);
         return server;
     };
 
     it('succeeds on baseline request', function(done) {
         var server = provisionServer();
-console.log(server);
         server.route({ method: 'GET', path: '/', handler: {
             oembed: {
                 title: 'Embed',
@@ -31,19 +32,137 @@ console.log(server);
             done();
         });
     });
-
-    it('requires a url parameter', function(done) {
+    it('limits height to maxheight', function(done) {
         var server = provisionServer();
         server.route({ method: 'GET', path: '/', handler: {
             oembed: {
                 title: 'Embed',
+                html: '<aside>Example Content</aside>',
+                height: 20
+            }
+        }});
+
+        server.inject('/?maxheight=10', function(response) {
+            expect(response.result.height).to.equal('10');
+            done();
+        });
+    });
+    it('limits width to maxwidth', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Embed',
+                html: '<aside>Example Content</aside>',
+                width: 20
+            }
+        }});
+
+        server.inject('/?maxwidth=10', function(response) {
+            expect(response.result.width).to.equal('10');
+            done();
+        });
+    });
+    it('may be of type "rich"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Embed',
+                html: '<aside>Example Content</aside>',
+                type: 'rich'
+            }
+        }});
+
+        server.inject('/', function(response) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('may be of type "photo"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Embed',
+                url: 'http://example.com/',
+                type: 'photo'
+            }
+        }});
+
+        server.inject('/', function(response) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('may be of type "link"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Embed',
+                type: 'link'
+            }
+        }});
+
+        server.inject('/', function(response) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('may be of type "video"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Video',
+                html: '<aside>Example Content</aside>',
+                type: 'video'
+            }
+        }});
+
+        server.inject('/', function(response) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('may not be of invited type "mixed"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Mixed',
+                html: '<aside>Example Content</aside>',
+                type: 'mixed'
+            }
+        }});
+
+        server.inject('/', function(response) {
+            expect(response.statusCode).to.equal(500);
+            done();
+        });
+    });
+    it('the default oEmbed type is "rich"', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Mixed',
                 html: '<aside>Example Content</aside>'
             }
         }});
 
         server.inject('/', function(response) {
-            expect(response.statusCode).to.equal(400);
+            expect(response.result.type).to.equal('rich');
             done();
         });
     });
+    it('does not support XML oEmbed response', function(done) {
+        var server = provisionServer();
+        server.route({ method: 'GET', path: '/', handler: {
+            oembed: {
+                title: 'Example',
+                html: '<aside>Example Content</aside>'
+            }
+        }});
+
+        server.inject('/?format=xml', function(response) {
+            expect(response.statusCode).to.equal(501);
+            done();
+        });
+    });
+  });
 });
